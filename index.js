d9c06225c26df1dbab26a5f0342a187c45433db3
sruthi2397 <mail2raji.cs@gmail.com>
2018-05-04 17:25:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+const functions = require('firebase-functions');
+
+const app = dialogflow({debug: true});
+
+app.intent('make_name', (conv, {color, number}) => { conv.close(`Alright, your silly name is ${color} ${number}! ` + `I hope you like it. See you next time.`);
+}
+);
+
+exports.SillyNameMaker = functions.https.onRequest(app);
