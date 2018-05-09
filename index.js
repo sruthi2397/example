094b713ebf15173eebf25308bb46acf1c8a90c2c
sruthi2397 <mail2raji.cs@gmail.com>
2018-05-09 17:26:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-'use strict';
-
-const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
 
-const app = dialogflow({debug: true});
+admin.initializeApp(functions.config().firebase);
 
-app.intent('make_name', (conv, {color, number}) => { conv.close(`Alright, your silly name is ${color} ${number}! ` + `I hope you like it. See you next time.`);
-}
-);
+var firestore = admin.firestore();
 
-exports.SillyNameMaker = functions.https.onRequest(app);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.webhook = functions.https.onRequest((request, response) => {
+
+    console.log("request.body.result.parameters: ", request.body.result.parameters);
+    // {
+    //     name: "john",
+    //     persons: "3"
+    //     ...
+    // }
+
+    let params = request.body.result.parameters;
+
+    firestore.collection('orders').add(params)
+        .then(() => {
+
+            response.send({
+                speech:
+                    `${params.name} your hotel booking request for ${params.RoomType} room is forwarded for
+                    ${params.persons} persons, we will contact you on ${params.email} soon`
+            });
+        })
+        .catch((e => {
+            
+            console.log("error: ", e);
+
+            response.send({
+                speech: "something went wrong when writing on database"
+            });
+        }))
+
+
+});
